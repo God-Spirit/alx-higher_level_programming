030095cbbde234d1a670e1d3bd234321d5982962
God-Spirit <amobiv3@gmail.com>
2022-06-22 02:44:55 -0700

    print div

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+able File  10 lines (9 sloc)  208 Bytes
+
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print('Inside result: {}'.format(div))
+        return div
